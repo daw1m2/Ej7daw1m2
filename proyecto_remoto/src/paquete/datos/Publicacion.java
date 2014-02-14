5766653c1995de4f419c841ab5d232a756b88192
pilar <pilarg@User1>
2014-02-14 17:46:44 +0100

    Primera carga del repositorio

---
+++ proyecto_remoto/src/paquete/datos/Publicacion.java
@@ -0,0 +1,26 @@
+package paquete.datos;
+
+public class Publicacion {
+    private String codigo;
+    private String titulo;
+    private int año;
+    
+    /* Constructor */
+    public Publicacion(String codigo,String titulo, int año){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.año = año;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+    
+    public String toString() {
+        return "código:" + codigo + " título: " + titulo + " año: " + año;
+    }
+}
