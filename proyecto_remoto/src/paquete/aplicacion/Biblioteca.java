5715a09040fcd09b295140941362f503e2a8ac10
admin <admin@IC10.cesbur>
2014-02-25 08:54:36 +0100

    Version preliminar (aplicacion vacía).

---
+++ proyecto_remoto/src/paquete/aplicacion/Biblioteca.java
@@ -0,0 +1,20 @@
+package paquete.aplicacion;
+
+public class Biblioteca {
+	
+	private Libro[] coleccionLibros;
+	private Revista[] coleccionRevistas;
+	
+	public Biblioteca(){
+		
+	}
+
+	public static void main(String[] args) {
+		// Crear objeto de la apliacion
+		new Biblioteca();
+		// Fin de la aplicacion
+		System.exit(0);
+
+	}
+
+}
